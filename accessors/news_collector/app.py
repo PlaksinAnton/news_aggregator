f4d451fea63a2ad6dc7a0d6f38781ca0310504b6
anton <aplaksin2000@gmail.com>
2024-11-21 17:19:00 +0200

    feature: add connection to the guardian api

--- accessors/news_collector/app.py
+++ accessors/news_collector/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, g, jsonify
+import requests
+import os
 
 
 app = Flask(__name__)
+
+api_key = os.getenv("GUARDIAN_API_KEY")
 
 # @app.before_request
 # def setup_session():
@@ -12,6 +16,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return "<p>Hello from app!</p>"
+
+@app.route('/request/<string:request>', methods=['GET'])
+def get_api_data(request):
+    base_url = 'https://content.guardianapis.com/search'
+    params = { 'q':request, 'api-key':api_key }
+    r = requests.get(url = base_url, params = params)
+    return jsonify(r.json())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8083)
