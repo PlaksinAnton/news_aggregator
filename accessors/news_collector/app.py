09258634b30e6c1d20ec9317c8cd3be8d1b11540
Anton <86431087+PlaksinAnton@users.noreply.github.com>
2024-11-23 15:33:02 +0200

    Feature/add news collector (#3)
    * feature: add new flask instance
    
    * feature: add connection to the guardian api
    
    * feature: add logic and logging to news collecter
    
    * refactor: better logging of news collector

---
+++ accessors/news_collector/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify, request
+import requests
+from datetime import datetime
+import os
+
+
+app = Flask(__name__)
+
+log_level = os.getenv("LOG_LEVEL", "INFO")
+app.logger.setLevel(log_level)
+
+API_KEY = os.getenv("GUARDIAN_API_KEY")
+SEARCH_URL = 'https://content.guardianapis.com/search'
+INITIAL_NUMBER_OF_PUBLICATIONS = 30
+RESULT_NUMBER_OF_PUBLICATIONS = 5
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "<p>Hello from app!</p>"
+
+@app.route('/get_news', methods=['POST'])
+def get_news():
+    app.logger.info('/get_news endpoint got called')
+    
+    request_data = request.get_json()
+    request_body = request_data.get("body")
+    if request_data is None or request_body is None:
+        app.logger.warning('endpoint got invalid request JSON')
+        return jsonify({'message': 'Invalid JSON'}), 400
+
+    try:
+        params = { 'q':request_body, 'lang':'en', 'page-size':INITIAL_NUMBER_OF_PUBLICATIONS, 'api-key':API_KEY }
+        news_json = requests.get(url = SEARCH_URL, params = params)
+    except Exception as e:
+        app.logger.error(f"{e}")
+        return jsonify({'error': 'Internal news request error'}), 500
+    else:
+        app.logger.info('successfull news request')
+
+    news_data = news_json.json()
+    if news_data.get("response", {}).get("total") is 0:
+        app.logger.warning('empty news page is returned from the API')
+        return jsonify({'empty_page': True, 'lateat_news': []}), 200
+    elif news_data.get("response", {}).get("total") is None:
+        app.logger.error('regular fields is missing in the news API response')
+        return jsonify({'error': 'Problems with news API'}), 500
+    
+    try:
+        latest_news_data = sorted(
+            news_data["response"]["results"], 
+            key=lambda x: datetime.strptime(x["webPublicationDate"], "%Y-%m-%dT%H:%M:%SZ"), 
+            reverse=True
+        )
+        latest_news_data = latest_news_data[:RESULT_NUMBER_OF_PUBLICATIONS]
+        lateat_news = [
+            {
+                "publication_date": article["webPublicationDate"],
+                "title": article["webTitle"],
+                "url": article["webUrl"]
+            }
+            for article in latest_news_data
+        ]
+    except Exception as e:
+        app.logger.error(f"{e}")
+        return jsonify({'error': 'Internal JSON parsing error'}), 500
+    else:
+        app.logger.info('JSON parsed successfully')
+    
+    return jsonify({'empty_page': False, 'lateat_news': lateat_news}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8083)
+    
