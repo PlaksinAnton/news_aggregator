0b3002de727dd969fea3da3133449c91c9dcc8a6
anton <aplaksin2000@gmail.com>
2024-11-24 00:34:02 +0200

    feature: add app user scaffold

--- db/users_db/entrypoint-initdb.d/init.sql
+++ db/users_db/entrypoint-initdb.d/init.sql
@@ -8,27 +8,27 @@ ALTER USER 'root'@'%' IDENTIFIED BY 'root';
 FLUSH PRIVILEGES;
 
 
-CREATE TABLE IF NOT EXISTS User (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    telephone VARCHAR(15)
-);
+-- CREATE TABLE IF NOT EXISTS User (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     name VARCHAR(100) NOT NULL,
+--     telephone VARCHAR(15)
+-- );
 
-CREATE TABLE IF NOT EXISTS Preference (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    preference VARCHAR(100) UNIQUE NOT NULL
-);
+-- CREATE TABLE IF NOT EXISTS Preference (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     preference VARCHAR(100) UNIQUE NOT NULL
+-- );
 
-CREATE TABLE IF NOT EXISTS User_Preferences (
-    user_id INT,
-    preference_id INT,
-    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
-    FOREIGN KEY (preference_id) REFERENCES Preference(id) ON DELETE CASCADE,
-    PRIMARY KEY (user_id, preference_id)
-);
+-- CREATE TABLE IF NOT EXISTS User_Preferences (
+--     user_id INT,
+--     preference_id INT,
+--     FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE,
+--     FOREIGN KEY (preference_id) REFERENCES Preference(id) ON DELETE CASCADE,
+--     PRIMARY KEY (user_id, preference_id)
+-- );
 
-INSERT INTO User (name, telephone) VALUES ('Anton', '0557712411');
-INSERT INTO Preference (preference) VALUES ('politics'), ('hight-tech'), ('stock market');
-INSERT INTO User_Preferences (user_id, preference_id) VALUES
-    (1, 1),
-    (1, 2);
+-- INSERT INTO User (name, telephone) VALUES ('Anton', '0557712411');
+-- INSERT INTO Preference (preference) VALUES ('politics'), ('hight-tech'), ('stock market');
+-- INSERT INTO User_Preferences (user_id, preference_id) VALUES
+--     (1, 1),
+--     (1, 2);
