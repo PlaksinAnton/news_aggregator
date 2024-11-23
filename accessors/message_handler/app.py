283a5e25971efd35089acc9a05eb91216b465d96
Anton <86431087+PlaksinAnton@users.noreply.github.com>
2024-11-23 15:29:16 +0200

    Feature/add message handler (#2)
    * feature: add logic to message handler
    
    * feature: add logging to message handler
    
    * refactor: better logging of message handler

---
+++ accessors/message_handler/app.py
@@ -0,0 +1,57 @@
+from flask import Flask, g, jsonify, request
+import mailer
+import os
+
+app = Flask(__name__)
+
+log_level = os.getenv("LOG_LEVEL", "INFO")
+app.logger.setLevel(log_level)
+# log_handler = logging.handlers.RotatingFileHandler('app.log', maxBytes=5*1024*1024, backupCount=2)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "<p>Hello from app!</p>"
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    app.logger.info('/send_message endpoint got called')
+    
+    message_data = request.get_json()
+    if message_data is None:
+        app.logger.warning('endpoint got invalid request JSON')
+        return jsonify({'message': 'Invalid JSON'}), 400
+
+    try:
+        service = mailer.authenticate_gmail()
+    except Exception as e:
+        app.logger.error(f"{e}")
+        return jsonify({'error': 'Internal email api authentication error'}), 500
+    else:
+        app.logger.info('successfull gmail authentication')
+
+    sender = "aplaksin691@gmail.com"
+    recipient = message_data.get("recipient")
+    subject = message_data.get("subject")
+    body = message_data.get("body")
+
+    if body is None or subject is None or recipient is None:
+        app.logger.warning('some JSON fields are empty')
+        return jsonify({'message': 'JSON does not contain all necessary fields'}), 400
+    app.logger.info('JSON parsing is done')
+
+    try:
+        mailer.send_email(service,
+                          sender,
+                          recipient,
+                          subject, 
+                          body)
+    except Exception as e:
+        app.logger.error(f"{e}")
+        return jsonify({'error': 'Internal email api sending error'}), 500
+    else:
+        app.logger.info('email is sent successfully')
+
+    return jsonify({'message': 'Success'}), 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8084)
---
+++ accessors/message_handler/mailer.py
@@ -0,0 +1,59 @@
+import os.path
+import base64
+from email.mime.text import MIMEText
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+
+def authenticate_gmail():
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If no valid credentials, initiate the OAuth flow
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    return build("gmail", "v1", credentials=creds)
+
+def create_message(sender, to, subject, body):
+    message = MIMEText(body)
+    message["to"] = to
+    message["from"] = sender
+    message["subject"] = subject
+    raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
+    return {"raw": raw}
+
+def send_email(service, sender, to, subject, body):
+    try:
+        message = create_message(sender, to, subject, body)
+        service.users().messages().send(userId="me", body=message).execute()
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+
+def main():
+    try:
+        service = authenticate_gmail()
+        sender = "aplaksin691@gmail.com"
+        recipient = "mihsham1@gmail.com"
+        subject = "Hello from Gmail API"
+        body = "This is a test email sent using the Gmail API."
+        send_email(service, sender, recipient, subject, body)
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+
+if __name__ == "__main__":
+    main()
